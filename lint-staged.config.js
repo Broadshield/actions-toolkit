c3f833130d916609e09f1914b85e85c3d96696b6
Jamie Nelson <jamie@wearsafe.com>
2022-11-29 13:38:09 -0500

    release(github-actions-core-typed-inputs): 🎸 cut release to 0.1.0

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   'tools/**/*.{js,jsx,cjs,mjs,ts,tsx}': [
-    'nx affected --target=lint --uncommitted --fix=true',
     'nx format:write --uncommitted',
   ],
   'packages/*/src/**/*.{ts,tsx}': [
@@ -8,6 +7,6 @@ module.exports = {
     'nx affected --target=test --uncommitted',
     'nx format:write --uncommitted',
   ],
-  'packages/*/*.{ts,tsx}': ['nx affected --target=lint --uncommitted --fix=true', 'nx format:write --uncommitted'],
-  '**/*.{md,yml}': ['nx affected --target=lint --uncommitted --fix=true', 'nx format:write --uncommitted'],
+  'packages/*/*.{ts,tsx}': ['nx format:write --uncommitted'],
+  '**/*.{md,yml}': ['nx format:write --uncommitted'],
 };
